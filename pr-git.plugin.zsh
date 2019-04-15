425376ece107d58b3845789915b8dc8ac2533cc3
Grigorii Horos <horosgrisa@gmail.com>
2019-04-16 02:10:51 +0300

    Fix git plugin

--- pr-git.plugin.zsh
+++ pr-git.plugin.zsh
@@ -8,34 +8,53 @@ GIT_SYMBOL=${GIT_SYMBOL:-''}
 
 
 _git-info() {
-  changes=$(git status --porcelain | head -1 2>/dev/null)
-  ref=$(command git symbolic-ref HEAD 2>/dev/null)
+  INDEX=$(command git status --porcelain -b 2> /dev/null)
+
+  git_changes=$(echo "$INDEX" | wc -l 2>/dev/null)
   if [[ $CLICOLOR = 1 ]]; then
-    
-    if [ ! -z "$changes" ]; then
+    if [[ "$git_changes" > "1" ]]; then
       git_status="%{$fg_bold[red]%}$GIT_SYMBOL%{$reset_color%}"
     else
       git_status="%{$fg_bold[green]%}$GIT_SYMBOL%{$reset_color%}"
     fi
-    
   else
-    
-    if [ ! -z "$changes" ]; then
+    if [[ "$git_changes" > "1" ]]; then
       git_status="-$GIT_SYMBOL"
     else
       git_status="+$GIT_SYMBOL"
     fi
-    
   fi
+
+  git_branch_name=$(echo "$INDEX" | head -1 2>/dev/null)
   if [[ $CLICOLOR = 1 ]]; then
-
-  git_branch="%{$fg_bold[yellow]%}${ref#refs/heads/}%{$reset_color%}"
+    git_branch=" %{$fg_bold[yellow]%}${git_branch_name#*/}%{$reset_color%}"
   else
-  git_branch="${ref#refs/heads/}"
-
+    git_branch=" ${git_branch_name#*/}"
   fi
 
-  echo "$git_status $git_branch"
+  git_untracked_number=$(echo "$INDEX" | command grep -E '^[ MARC]M ' | wc -l)
+  if [[ "$git_untracked_number" == 0 ]]; then
+      git_untracked=''
+  else
+    if [[ $CLICOLOR = 1 ]]; then
+      git_untracked=" %{$fg_bold[magenta]%}?${git_untracked_number}%{$reset_color%}"
+    else
+      git_untracked=" ?${git_untracked_number}"
+    fi
+  fi
+
+  git_added_number=$(echo "$INDEX" | command grep -E '^\?\? ' | wc -l)
+  if [[ "$git_added_number" == 0 ]]; then
+      git_added=''
+  else
+    if [[ $CLICOLOR = 1 ]]; then
+      git_added=" %{$fg_bold[red]%}+${git_added_number}%{$reset_color%}"
+    else
+      git_added=" +${git_added_number}"
+    fi
+  fi
+
+  echo "$git_status$git_branch$git_untracked$git_added"
   
 }
 
