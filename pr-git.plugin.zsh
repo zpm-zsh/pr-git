682dd903437b36c09a3952233741af98fa34366c
Grigorii Horos <horosgrisa@gmail.com>
2019-07-16 09:26:57 +0300

    Fix symbols

--- pr-git.plugin.zsh
+++ pr-git.plugin.zsh
@@ -4,18 +4,18 @@ GIT_STATUS_PREFIX=${GIT_STATUS_PREFIX:-' '}
 GIT_STATUS_SUFIX=${GIT_STATUS_SUFIX:-''}
 
 GIT_STATUS_SYMBOL=${GIT_STATUS_SYMBOL:-''}
-GIT_STATUS_MODIFIEED="${GIT_STATUS_MODIFIEED="~"}"
+GIT_STATUS_MODIFIEED="${GIT_STATUS_MODIFIEED="±"}"
 GIT_STATUS_ADDED="${GIT_STATUS_ADDED="+"}"
 GIT_STATUS_DELETED="${GIT_STATUS_DELETED="-"}"
-GIT_STATUS_STAGED="${GIT_STATUS_STAGED="▸"}"
+GIT_STATUS_STAGED="${GIT_STATUS_STAGED="→"}"
 GIT_STATUS_AHEAD="${GIT_STATUS_AHEAD="⇡"}"
 GIT_STATUS_BEHIND="${GIT_STATUS_BEHIND="⇣"}"
 
 
-DEPENDENCES_ZSH+=( zpm-zsh/helpers zpm-zsh/background-functions )
+DEPENDENCES_ZSH+=( zpm-zsh/helpers zpm-zsh/background-functions zpm-zsh/colors )
 
 if which zpm >/dev/null; then
-  zpm zpm-zsh/helpers zpm-zsh/background-functions
+  zpm zpm-zsh/helpers zpm-zsh/background-functions zpm-zsh/colors
 fi
 
 _git-info() {
@@ -25,9 +25,9 @@ _git-info() {
   git_changes=$(echo "$INDEX" | wc -l 2>/dev/null)
   if [[ $CLICOLOR = 1 ]]; then
     if [[ "$git_changes" > "1" ]]; then
-      git_status="%{$fg_bold[red]%}$GIT_STATUS_SYMBOL%{$reset_color%}"
+      git_status="%{$c[red]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
     else
-      git_status="%{$fg_bold[green]%}$GIT_STATUS_SYMBOL%{$reset_color%}"
+      git_status="%{$c[green]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
     fi
   else
     if [[ "$git_changes" > "1" ]]; then
@@ -39,7 +39,7 @@ _git-info() {
 
   ref=$(command git symbolic-ref HEAD 2>/dev/null)
   if [[ $CLICOLOR = 1 ]]; then
-    git_branch=" %{$fg_bold[yellow]%}${ref#refs/heads/}%{$reset_color%}"
+    git_branch=" %{$c[yellow]$c_bold%}${ref#refs/heads/}%{$c_reset%}"
   else
     git_branch=" ${ref#refs/heads/}"
   fi
@@ -49,7 +49,7 @@ _git-info() {
       git_modified=''
   else
     if [[ $CLICOLOR = 1 ]]; then
-      git_modified=" %{$fg_bold[magenta]%}${GIT_STATUS_MODIFIEED}${git_modified_number}%{$reset_color%}"
+      git_modified=" %{$c[magenta]$c_dim$c_bold%}${GIT_STATUS_MODIFIEED}%{$c_reset$c[magenta]$c_bold%}${git_modified_number}%{$c_reset%}"
     else
       git_modified=" ${GIT_STATUS_MODIFIEED}${git_modified_number}"
     fi
@@ -60,7 +60,7 @@ _git-info() {
       git_added=''
   else
     if [[ $CLICOLOR = 1 ]]; then
-      git_added=" %{$fg_bold[blue]%}${GIT_STATUS_ADDED}${git_added_number}%{$reset_color%}"
+      git_added=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_ADDED}%{$c_reset$c[blue]$c_bold%}${git_added_number}%{$c_reset%}"
     else
       git_added=" ${GIT_STATUS_ADDED}${git_added_number}"
     fi
@@ -71,7 +71,7 @@ _git-info() {
       git_deleted=''
   else
     if [[ $CLICOLOR = 1 ]]; then
-      git_deleted=" %{$fg_bold[red]%}${GIT_STATUS_DELETED}${git_deleted_number}%{$reset_color%}"
+      git_deleted=" %{$c[red]$c_dim$c_bold%}${GIT_STATUS_DELETED}%{$c_reset$c[red]$c_bold%}${git_deleted_number}%{$c_reset%}"
     else
       git_deleted=" ${GIT_STATUS_DELETED}${git_deleted_number}"
     fi
@@ -82,7 +82,7 @@ _git-info() {
       git_staged=''
   else
     if [[ $CLICOLOR = 1 ]]; then
-      git_staged=" %{$fg_bold[cyan]%}${GIT_STATUS_STAGED}${git_staged_number}%{$reset_color%}"
+      git_staged=" %{$c[cyan]$c_dim$c_bold%}${GIT_STATUS_STAGED}%{$c_reset$c[cyan]$c_bold%}${git_staged_number}%{$c_reset%}"
     else
       git_staged=" ${GIT_STATUS_STAGED}${git_staged_number}"
     fi
@@ -93,7 +93,7 @@ _git-info() {
       git_ahead=''
   else
     if [[ $CLICOLOR = 1 ]]; then
-      git_ahead=" %{$fg_bold[blue]%}${GIT_STATUS_AHEAD}${git_ahead_number}%{$reset_color%}"
+      git_ahead=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_AHEAD}%{$c_reset$c[blue]$c_bold%}${git_ahead_number}%{$c_reset%}"
     else
       git_ahead=" ${GIT_STATUS_AHEAD}${git_ahead_number}"
     fi
@@ -104,14 +104,13 @@ _git-info() {
       git_behind=''
   else
     if [[ $CLICOLOR = 1 ]]; then
-      git_behind=" %{$fg_bold[cyan]%}${GIT_STATUS_BEHIND}${git_behind_number}%{$reset_color%}"
+      git_behind=" %{$c[cyan]$c_dim$c_bold%}${GIT_STATUS_BEHIND}%{$c_reset$c[cyan]$c_bold%}${git_behind_number}%{$c_reset%}"
     else
       git_behind=" ${GIT_STATUS_BEHIND}${git_behind_number}"
     fi
   fi
 
   echo "$git_status$git_branch$git_modified$git_added$git_deleted$git_staged$git_ahead$git_behind"
-  
 }
 
 _git_prompt() {
