36a0bdc30f8676c5335cde1ef3ce07f12c18fae3
Grigorii Horos <horosgrisa@gmail.com>
2020-01-24 16:28:20 +0200

    Move to zsh-git-cal-status-cpp

--- pr-git.plugin.zsh
+++ pr-git.plugin.zsh
@@ -1,79 +1,96 @@
 #!/usr/bin/env zsh
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+base_dir=${0:h}
 
 GIT_STATUS_PREFIX=${GIT_STATUS_PREFIX:-' '}
 GIT_STATUS_SUFIX=${GIT_STATUS_SUFIX:-''}
 
 GIT_STATUS_SYMBOL=${GIT_STATUS_SYMBOL:-''}
-GIT_STATUS_MODIFIEED="${GIT_STATUS_MODIFIEED="±"}"
-GIT_STATUS_ADDED="${GIT_STATUS_ADDED="+"}"
-GIT_STATUS_DELETED="${GIT_STATUS_DELETED="-"}"
-GIT_STATUS_STAGED="${GIT_STATUS_STAGED="→"}"
+GIT_STATUS_STAGED="${GIT_STATUS_STAGED="→"}" 
+GIT_STATUS_CONFLICTS="${GIT_STATUS_CONFLICTS="≠"}"
+GIT_STATUS_CHANGED="${GIT_STATUS_CHANGED="±"}" 
+GIT_STATUS_BEHIND="${GIT_STATUS_BEHIND="↓"}" 
 GIT_STATUS_AHEAD="${GIT_STATUS_AHEAD="↑"}"
-GIT_STATUS_BEHIND="${GIT_STATUS_BEHIND="↓"}"
+GIT_STATUS_UNTRACKED="${GIT_STATUS_UNTRACKED="+"}"
+
 
 
 DEPENDENCES_ZSH+=( zpm-zsh/helpers zpm-zsh/background zpm-zsh/colors )
 
-if command -v zpm >/dev/null; then
+if (( $+functions[zpm] )); then 
   zpm zpm-zsh/helpers,inline zpm-zsh/background,inline zpm-zsh/colors,inline
+fi
+
+if (( ! $+commands[git-status] )); then
+  (
+    cd "${base_dir}/zsh-git-cal-status-cpp/"
+    make
+    cp git-status.bin ../bin/git-status
+  )
 fi
 
 _git-info() {
   setopt extendedglob
   
-  declare -a INDEX; INDEX=( ${(f)"$(command git status --no-renames --ignore-submodules --porcelain -b 2> /dev/null)"} )
+  command git-status --whoami grisa --pwd-dir . --refresh-sec 3 2> /dev/null | read -A __CURRENT_GIT_STATUS
+
+  GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
+  GIT_AHEAD=$__CURRENT_GIT_STATUS[2]
+  GIT_BEHIND=$__CURRENT_GIT_STATUS[3]
+  GIT_STAGED=$__CURRENT_GIT_STATUS[4]
+  GIT_CONFLICTS=$__CURRENT_GIT_STATUS[5]
+  GIT_CHANGED=$__CURRENT_GIT_STATUS[6]
+  GIT_UNTRACKED=$__CURRENT_GIT_STATUS[7]
+  GIT_SECONDS_OR_ERROR=$__CURRENT_GIT_STATUS[8]
+
   
-  if [[ "${#INDEX}" != "1" ]]; then
-    git_status="%{$c[red]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
+  if [[ $(( $GIT_UNTRACKED +  $GIT_CHANGED + $GIT_CONFLICTS + $GIT_STAGED )) != "0" ]]; then
+    GIT_STATUS="%{$c[red]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
   else
-    git_status="%{$c[green]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
+    GIT_STATUS="%{$c[green]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
   fi
   
-  git_branch=" %{$c[yellow]$c_bold%}${${INDEX[1]#\##\ *}%\.\.\.*}%{$c_reset%}"
+  GIT_BRANCH=" %{$c[yellow]$c_bold%}${GIT_BRANCH}%{$c_reset%}"
   
-  declare -a git_modified_number; git_modified_number=( ${(M)INDEX:#[ MARC]M\ *} )
-  if [[ -z "${git_modified_number}" ]]; then
-    git_modified=''
+  if [ "${GIT_AHEAD}" -eq "0" ]; then
+    GIT_AHEAD=''
   else
-    git_modified=" %{$c[magenta]$c_dim$c_bold%}${GIT_STATUS_MODIFIEED}%{$c_reset$c[magenta]$c_bold%}${#git_modified_number}%{$c_reset%}"
+    GIT_AHEAD=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_AHEAD}%{$c_reset$c[blue]$c_bold%}${GIT_AHEAD}%{$c_reset%}"
+  fi
+
+  if [ "${GIT_BEHIND}" -eq "0" ]; then
+    GIT_BEHIND=''
+  else
+    GIT_BEHIND=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_BEHIND}%{$c_reset$c[blue]$c_bold%}${GIT_BEHIND}%{$c_reset%}"
+  fi
+
+  if [ "${GIT_STAGED}" -eq "0" ]; then
+    GIT_STAGED=''
+  else
+    GIT_STAGED=" %{$c[cyan]$c_dim$c_bold%}${GIT_STATUS_STAGED}%{$c_reset$c[cyan]$c_bold%}${GIT_STAGED}%{$c_reset%}"
+  fi
+
+  if [ "${GIT_CONFLICTS}" -eq "0" ]; then
+    GIT_CONFLICTS=''
+  else
+    GIT_CONFLICTS=" %{$c[red]$c_dim$c_bold%}${GIT_STATUS_CONFLICTS}%{$c_reset$c[red]$c_bold%}${GIT_STAGED}%{$c_reset%}"
+  fi
+
+  if [ "${GIT_CHANGED}"  -eq "0" ]; then
+    GIT_CHANGED=''
+  else
+    GIT_CHANGED=" %{$c[magenta]$c_dim$c_bold%}${GIT_STATUS_CHANGED}%{$c_reset$c[magenta]$c_bold%}${GIT_CHANGED}%{$c_reset%}"
   fi
   
-  declare -a git_added_number; git_added_number=( ${(M)INDEX:#\?\?\ *} )
-  if [[ -z "${git_added_number}" ]]; then
-    git_added=''
+  if [ "${GIT_UNTRACKED}" -eq "0" ]; then
+    GIT_UNTRACKED=''
   else
-    git_added=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_ADDED}%{$c_reset$c[blue]$c_bold%}${#git_added_number}%{$c_reset%}"
+    GIT_UNTRACKED=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_UNTRACKED}%{$c_reset$c[blue]$c_bold%}${GIT_UNTRACKED}%{$c_reset%}"
   fi
+    
   
-  declare -a git_deleted_number; git_deleted_number=( ${(M)INDEX:#[ MARC]D\ *} )
-  if [[ -z "${git_deleted_number}" ]]; then
-    git_deleted=''
-  else
-    git_deleted=" %{$c[red]$c_dim$c_bold%}${GIT_STATUS_DELETED}%{$c_reset$c[red]$c_bold%}${#git_deleted_number}%{$c_reset%}"
-  fi
-  
-  declare -a git_staged_number; git_staged_number=( ${(M)INDEX:#[MARC]*} )
-  if [[ -z "${git_staged_number}" ]]; then
-    git_staged=''
-  else
-    git_staged=" %{$c[cyan]$c_dim$c_bold%}${GIT_STATUS_STAGED}%{$c_reset$c[cyan]$c_bold%}${#git_staged_number}%{$c_reset%}"
-  fi
-  
-  local git_ahead_number="${${INDEX[@]##*ahead\ }%%[\,\]]*}"
-  if [[ "$INDEX[@]" != *'ahead'* ]]; then
-    git_ahead=''
-  else
-    git_ahead=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_AHEAD}%{$c_reset$c[blue]$c_bold%}${git_ahead_number}%{$c_reset%}"
-  fi
-  
-  local git_behind_number="${${INDEX[@]##*behind\ }%%[\,\]]*}"
-  if [[ "$INDEX[@]" != *"behind"* ]]; then
-    git_behind=''
-  else
-    git_behind=" %{$c[cyan]$c_dim$c_bold%}${GIT_STATUS_BEHIND}%{$c_reset$c[cyan]$c_bold%}${git_behind_number}%{$c_reset%}"
-  fi
-  
-  echo "$git_status$git_branch$git_modified$git_added$git_deleted$git_staged$git_ahead$git_behind"
+  echo "${GIT_STATUS}${GIT_BRANCH}${GIT_CHANGED}${GIT_UNTRACKED}${GIT_CONFLICTS}${GIT_STAGED}${GIT_AHEAD}${GIT_BEHIND}"
 }
 
 _git_prompt() {
