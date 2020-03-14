29c2aefd260dfd4a4aee367906062cf23ec87da1
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 02:57:26 +0200

    Optimizations

--- pr-git.plugin.zsh
+++ pr-git.plugin.zsh
@@ -11,34 +11,34 @@ GIT_STATUS_SUFIX=${GIT_STATUS_SUFIX:-''}
 typeset -g GIT_STATUS_SYMBOL
 GIT_STATUS_SYMBOL=${GIT_STATUS_SYMBOL:-''}
 typeset -g GIT_STATUS_STAGED
-GIT_STATUS_STAGED="${GIT_STATUS_STAGED:-"→"}" 
+GIT_STATUS_STAGED="${GIT_STATUS_STAGED:-"→"}"
 typeset -g GIT_STATUS_CONFLICTS
 GIT_STATUS_CONFLICTS="${GIT_STATUS_CONFLICTS:-"≠"}"
 typeset -g GIT_STATUS_CHANGED
-GIT_STATUS_CHANGED="${GIT_STATUS_CHANGED:-"±"}" 
+GIT_STATUS_CHANGED="${GIT_STATUS_CHANGED:-"±"}"
 typeset -g GIT_STATUS_BEHIND
-GIT_STATUS_BEHIND="${GIT_STATUS_BEHIND:-"↓"}" 
+GIT_STATUS_BEHIND="${GIT_STATUS_BEHIND:-"↓"}"
 typeset -g GIT_STATUS_AHEAD
 GIT_STATUS_AHEAD="${GIT_STATUS_AHEAD:-"↑"}"
 typeset -g GIT_STATUS_UNTRACKED
 GIT_STATUS_UNTRACKED="${GIT_STATUS_UNTRACKED:-"+"}"
 
-
+typeset -g pr_git
 
 DEPENDENCES_ZSH+=( zpm-zsh/helpers zpm-zsh/background zpm-zsh/colors )
 
-if (( $+functions[zpm] )); then 
+if (( $+functions[zpm] )); then
   zpm zpm-zsh/helpers,inline zpm-zsh/background,inline zpm-zsh/colors,inline
 fi
 
-_git-info() {
+_git-function info() {
   if (( ! $+commands[git-status] )); then
     echo Please, install git-status from https://gitlab.com/cosurgi/zsh-git-cal-status-cpp
     return
   fi
 
   # setopt extendedglob
-  
+
   command git-status --whoami $USER --pwd-dir . --refresh-sec 3 2> /dev/null | read -A __CURRENT_GIT_STATUS
 
   GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
@@ -50,15 +50,15 @@ _git-info() {
   GIT_UNTRACKED=$__CURRENT_GIT_STATUS[7]
   GIT_SECONDS_OR_ERROR=$__CURRENT_GIT_STATUS[8]
 
-  
+
   if [[ $(( $GIT_UNTRACKED +  $GIT_CHANGED + $GIT_CONFLICTS + $GIT_STAGED )) != "0" ]]; then
     GIT_STATUS="%{$c[red]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
   else
     GIT_STATUS="%{$c[green]$c_dim%}$GIT_STATUS_SYMBOL%{$c_reset%}"
   fi
-  
+
   GIT_BRANCH=" %{$c[yellow]$c_bold%}${GIT_BRANCH}%{$c_reset%}"
-  
+
   if [ "${GIT_AHEAD}" -eq "0" ]; then
     GIT_AHEAD=''
   else
@@ -88,31 +88,26 @@ _git-info() {
   else
     GIT_CHANGED=" %{$c[magenta]$c_dim$c_bold%}${GIT_STATUS_CHANGED}%{$c_reset$c[magenta]$c_bold%}${GIT_CHANGED}%{$c_reset%}"
   fi
-  
+
   if [ "${GIT_UNTRACKED}" -eq "0" ]; then
     GIT_UNTRACKED=''
   else
     GIT_UNTRACKED=" %{$c[blue]$c_dim$c_bold%}${GIT_STATUS_UNTRACKED}%{$c_reset$c[blue]$c_bold%}${GIT_UNTRACKED}%{$c_reset%}"
   fi
-    
-  
+
+
   echo "${GIT_STATUS}${GIT_BRANCH}${GIT_CHANGED}${GIT_UNTRACKED}${GIT_CONFLICTS}${GIT_STAGED}${GIT_AHEAD}${GIT_BEHIND}"
 }
 
-_git_prompt() {
-  typeset -g pr_git
-  typeset -g pr_git_old
+function _git_prompt() {
   if is-recursive-exist .git; then
     pr_git_old="$pr_git"
     pr_git="$GIT_STATUS_PREFIX$(_git-info 2>/dev/null)$GIT_STATUS_SUFIX"
-    if [[ ! "$pr_git_old" == "$pr_git" ]]; then
-      zle && zle reset-prompt
-    fi
   else
     pr_git=""
   fi
 }
 
-_git_prompt
+autoload -Uz add-zsh-hook
 add-zsh-hook precmd _git_prompt
-add-zsh-hook background _git_prompt
+_git_prompt
