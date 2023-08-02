940447d15118be411b12e3918aa40bd4b648e012
Grigorii Horos <horosgrisa@gmail.com>
2023-08-03 00:54:41 +0300

    Remove prefix/suffix

--- pr-git.plugin.zsh
+++ pr-git.plugin.zsh
@@ -7,9 +7,6 @@ if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
 
-: ${GIT_STATUS_PREFIX:=' '}
-: ${GIT_STATUS_SUFIX:=''}
-
 : ${GIT_STATUS_STAGED:='→'}
 : ${GIT_STATUS_CONFLICTS:='≠'}
 : ${GIT_STATUS_CHANGED:='±'}
@@ -23,7 +20,7 @@ typeset -g pr_git=''
 
 function _git_prompt() {
   if is-recursive-exist .git; then
-    pr_git="$GIT_STATUS_PREFIX$(git-prompt 2>/dev/null)$GIT_STATUS_SUFIX"
+    pr_git=" $(git-prompt 2>/dev/null)"
   else
     pr_git=""
   fi
